b3bc36261e32117ab5ef8e961147ff5367754585
ychleo102615 <leo102615@gmail.com>
2023-06-05 16:01:39 +0800

    feat: reverse y axis

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ void main() {
   // convert from 0->2 to -1->+1 (clip space)
   vec2 clipSpace = zeroToTwo - 1.0;
 
-  gl_Position = vec4(clipSpace, 0, 1);
+  gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
 }
 `;
 
